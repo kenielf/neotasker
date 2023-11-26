c3b51823212a28aa3cd9e9ce634d74e893b6e767
kenielf <chrystian.keni.github@gmail.com>
2023-11-26 05:00:29 -0300

    Added proper JavaDoc support, fixed some of the documentation and bumped version.

--- src/main/java/com/neotasker/Main.java
+++ src/main/java/com/neotasker/Main.java
@@ -1,6 +1,14 @@
 package com.neotasker;
 
+/**
+ * This is the application's main class.
+ */
 public class Main {
+    /**
+     * The starting point of the program. This method is responsible for the 
+     * control of the functions at runtime.
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
