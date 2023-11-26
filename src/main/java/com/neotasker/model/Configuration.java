84d4d2f5133643bbea5dfc833903f6cbd03df989
kenielf <chrystian.keni.github@gmail.com>
2023-11-26 13:44:56 -0300

    Added code documentation for the models and bumped version.

--- src/main/java/com/neotasker/model/Configuration.java
+++ src/main/java/com/neotasker/model/Configuration.java
@@ -1,14 +1,29 @@
 package com.neotasker.model;
 
+/**
+ * This class is responsible for storing the values for the software's 
+ * configuration.
+ */
 public class Configuration {
     private String scalingPercentage;
     private String uiTheme;
     private String iconTheme;
     private Aliasing aliasingType;
 
+    /**
+     * Instantiates an empty configuration. This is meant to be filled later.
+     */
     public Configuration() {
     }
 
+    /**
+     * Instantiates a complete configuration.
+     *
+     * @param scalingPercentage sets the ui scaling percentage, example: "200%".
+     * @param uiTheme is either "light" or "dark".
+     * @param iconTheme same as uiTheme - "light" or "dark".
+     * @param aliasingType use {@link Aliasing} to set UI Aliasing
+     */
     public Configuration(String scalingPercentage, String uiTheme, String iconTheme, Aliasing aliasingType) {
         this.scalingPercentage = scalingPercentage;
         this.uiTheme = uiTheme;
@@ -16,34 +31,74 @@ public class Configuration {
         this.aliasingType = aliasingType;
     }
 
+    /**
+     * UI scaling setter.
+     *
+     * @param scalingPercentage is a string starting with a number and ending with a "%".
+     */
     public void setScalingPercentage(String scalingPercentage) {
         this.scalingPercentage = scalingPercentage;
     }
 
+    /**
+     * UI scaling getter.
+     *
+     * @return the percentage as a string.
+     */
     public String getScalingPercentage() {
         return scalingPercentage;
     }
 
+    /**
+     * UI theme setter.
+     *
+     * @param uiTheme can be either "light" or "dark". Anything else will break.
+     */
     public void setUiTheme(String uiTheme) {
         this.uiTheme = uiTheme;
     }
     
+    /**
+     * UI theme getter.
+     *
+     * @return returns the ui theme.
+     */
     public String getUiTheme() {
         return uiTheme;
     }
 
+    /**
+     * Icon theme setter.
+     *
+     * @param iconTheme same as {@link setUiTheme}, can be ither "light" or "dark".
+     */
     public void setIconTheme(String iconTheme) {
         this.iconTheme = iconTheme;
     }
 
+    /**
+     * Icon theme getter.
+     *
+     * @return returns the icon theme.
+     */
     public String getIconTheme() {
         return iconTheme;
     }
 
+    /**
+     * Aliasing type setter.
+     *
+     * @param aliasingType uses {@link Aliasing}.
+     */
     public void setAliasingType(Aliasing aliasingType) {
         this.aliasingType = aliasingType;
     }
 
+    /**
+     * Aliasing type getter.
+     *
+     * @return the type of aliasing.
+     */
     public Aliasing getAliasingType() {
         return aliasingType;
     }
--- src/main/java/com/neotasker/model/Theme.java
+++ src/main/java/com/neotasker/model/Theme.java
@@ -11,24 +11,52 @@ import com.formdev.flatlaf.fonts.inter.FlatInterFont;
 import com.formdev.flatlaf.intellijthemes.FlatArcDarkOrangeIJTheme;
 import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
 
+/**
+ * This class is responsible for setting and managing theme properties. This 
+ * class must not be instantiated.
+ */
 public class Theme {
+    /** This is the property key related to the UI scaling. */
     public static final String SCALING_PERCENTAGE_KEY = "flatlaf.uiScale";
+    /** This is the minimum value for the UI scaling. */
     public static final String SCALING_MINIMUM = "25%";
+    /** This is the default value for the UI scaling. */
     public static final String SCALING_DEFAULT = "100%";
+    /** This is the maximum value for the UI scaling. */
     public static final String SCALING_MAXIMUM = "200%";
+    /** This is the property key related to the UI theme. */
     public static final String UI_THEME_KEY = "UI_THEME";
+    /** This is the default UI theme. */
     public static final String UI_THEME_DEFAULT = "light";
+    /** This is the property key related to the icon theme. */
     public static final String ICON_THEME_KEY = "ICON_THEME";
+    /** This is the default icon theme */
     public static final String ICON_THEME_DEFAULT = "dark";
+    /** This is the default path for the application logo. */
     public static final String LOGO_ICON_PATH = "/icons/logo/neotasker.png";
+    /** This is the property key related to the aliasing type. */
     public static final String ALIASING_CUSTOM_KEY = "ALIASING_TYPE";
+    /** This is the property key related to the system synchronization of the aliasing type. */
     public static final String ALIASING_SYSTEM_SYNC_KEY = "awt.useSystemAAFontSettings";
+    /** This is the property key related to the toggle of the aliasing type */
     public static final String ALIASING_SWING_TOGGLE_KEY = "swing.aatext";
+    /** This is the default aliasing type */
     public static final Aliasing ALIASING_DEFAULT = Aliasing.SYSTEM;
 
+    /**
+     * Does nothing when instantiated.
+     */
     Theme() {
     }
 
+    /**
+     * Initializes the theme settings for the entire application.
+     *
+     * @param scalingPercentage same as Configuration.setAntiAliasing.
+     * @param uiTheme same as Configuration.setUiTheme.
+     * @param iconTheme same as Configuration.iconTheme.
+     * @param aliasingType same as Configuration.aliasingType.
+     */
     public static void Initialize(String scalingPercentage, String uiTheme, String iconTheme, Aliasing aliasingType) {
         setScalingPercentage(scalingPercentage);
         setDefaultFonts();
@@ -42,30 +70,61 @@ public class Theme {
         setFlatLafPreferences();
     }
 
+    /**
+     * Scaling percentage setter for the whole application.
+     *
+     * @param percentage the value of the scaling percentage as a string with a "%".
+     */
     public static void setScalingPercentage(String percentage) {
         System.setProperty(SCALING_PERCENTAGE_KEY, percentage);
     }
 
+    /**
+     * Scaling percentage getter for the whole application.
+     *
+     * @return the value of the scaling percentage as a string.
+     */
     public static String getScalingPercentage() {
         return System.getProperty(SCALING_PERCENTAGE_KEY);
     }
 
+    /**
+     * UI theme setter.
+     *
+     * @param theme can be either "light" or "dark" - same as Configuration.
+     */
     public static void setUiTheme(String theme) {
         System.setProperty(UI_THEME_KEY, theme);
     }
 
+    /**
+     * UI theme getter.
+     *
+     * @return the UI theme as a string.
+     */
     public static String getUiTheme() {
         return System.getProperty(UI_THEME_KEY);
     }
 
+    /**
+     * Icon theme setter.
+     *
+     * @param theme can be either "light" or "dark" - same as Configuration.
+     */
     public static void setIconTheme(String theme) {
         System.setProperty(ICON_THEME_KEY, theme);
     }
 
+    /**
+     * Icon theme getter.
+     */
     public static String getIconTheme() {
         return System.getProperty(ICON_THEME_KEY);
     }
 
+    /**
+     * Sets the UI theme to "light".
+     */
     public static void setLightUiTheme() {
         try {
             setUiTheme("light");
@@ -75,6 +134,9 @@ public class Theme {
         }
     }
 
+    /**
+     * Sets the UI theme to "dark".
+     */
     public static void setDarkUiTheme() {
         try {
             setUiTheme("dark");
@@ -84,6 +146,11 @@ public class Theme {
         }
     }
 
+    /**
+     * Aliasing type setter.
+     *
+     * @param option Uses {@link Aliasing} - same as Configuration.
+     */
     public static void setAntiAliasing(Aliasing option) {
         switch (option) {
             default:
@@ -106,15 +173,29 @@ public class Theme {
         }
     }
 
+    /**
+     * Aliasing type getter.
+     *
+     * @return the aliasing type with {@link Aliasing}.
+     */
     public static String getAntiAliasing() {
         return System.getProperty(ALIASING_CUSTOM_KEY);
     }
 
+    /**
+     * Get the path of an icon for 'GetResource()'.
+     *
+     * @param label the file name without extension.
+     * @return the full relative path to the file with the extension added.
+     */
     public static String getIcon(String label) {
         String iconPath = "/icons/" + getIconTheme() + "/" + label + ".png";
         return iconPath;
     }
 
+    /**
+     * Sets the default font for the whole application
+     */
     public static void setDefaultFonts() {
         FlatInterFont.install();
         FlatLaf.setPreferredFontFamily( FlatInterFont.FAMILY );
@@ -122,6 +203,9 @@ public class Theme {
         FlatLaf.setPreferredSemiboldFontFamily( FlatInterFont.FAMILY_SEMIBOLD );
     }
 
+    /**
+     * Sets the FlatLaf preferences, can be customized by changing the fields.
+     */
     public static void setFlatLafPreferences() {
         //UIManager.put( "Button.arc", 999 );
         //UIManager.put( "Component.arc", 999 );
